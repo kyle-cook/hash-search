a115907b21541659da1e423fd9685a7021d977e2
Kyle Cook <thothra@tachikoma.local>
2019-02-03 23:27:00 -0600

    POC working, able to scan local redis db for generated hashes

--- lib/hash-search.rb
+++ lib/hash-search.rb
@@ -3,29 +3,12 @@ require_relative 'util/names.rb'
 require_relative 'util/reason.rb'
 require_relative 'util/status.rb'
 
+require 'json'
 require 'redis'
 require 'sinatra'
-
-configure do
-    set :bind, '0.0.0.0'
-    set :port, 80
-end
+require 'sinatra/json'
 
 module HashSearch
-
-    class HashSearchApp < Sinatra::Base
-        configure do
-            set :bind, '0.0.0.0'
-            set :port, 80
-        end
-
-        redis = Redis.new
-
-        get '/' do
-            erb :index
-        end
-
-    end
 
     def RandomEntry
         [
@@ -36,6 +19,51 @@ module HashSearch
         ]
     end
 
+    module_function :RandomEntry
+
+    class HashSearchApp < Sinatra::Base
+        configure do
+            set :bind, '0.0.0.0'
+            set :port, 8080
+        end
+
+        @@redis
+
+        before do 
+            @@redis = Redis.new
+            """
+            for i in 0..1000000
+                hash = HashSearch.RandomEntry
+                @@redis.set(hash[0], hash[1..3].to_json)
+            end
+            """
+        end
+
+        get '/' do
+            erb :index
+        end
+
+        get '/favicon.ico' do
+            nil
+        end
+
+        get '/count' do
+            #32 MD5
+            #40 SHA1
+            #64 SHA2
+            @@redis.keys("*").count
+        end
+
+        get '/:type/:hash' do |type, hash|
+            json @@redis.scan(0, :match=>"#{type.upcase}:*#{hash.upcase}*", :count=>10000000)
+        end
+
+        get '/match/:type/:hash' do |type, hash|
+            json JSON.parse(@@redis.get("#{type.upcase}:#{hash.upcase}"))
+        end
+
+    end
+
 end
 
-#HashSearch::HashSearchApp.run!
+HashSearch::HashSearchApp.run!
--- lib/util/hashes.rb
+++ lib/util/hashes.rb
@@ -8,11 +8,11 @@ module HashSearch
 
             case rand 3
             when 0 then
-                return Digest::MD5.new.update(rand.to_s).hexdigest.upcase
+                return "MD5:#{Digest::MD5.new.update(rand.to_s).hexdigest.upcase}"
             when 1 then
-                return Digest::SHA1.new.update(rand.to_s).hexdigest.upcase
+                return "SHA1:#{Digest::SHA1.new.update(rand.to_s).hexdigest.upcase}"
             when 2 then
-                return Digest::SHA2.new.update(rand.to_s).hexdigest.upcase
+                return "SHA2:#{Digest::SHA2.new.update(rand.to_s).hexdigest.upcase}"
             end
 
         end
